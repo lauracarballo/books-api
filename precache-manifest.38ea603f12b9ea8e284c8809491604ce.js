af157041770dfd568030c7c75f61eb7078cf105f
Laura <laura.carballo14@gmail.com>
2020-11-30 15:19:54 +1100

    Updates

---
+++ precache-manifest.38ea603f12b9ea8e284c8809491604ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3130e547369b7d31c6d6fc52939470",
+    "url": "/books-api/index.html"
+  },
+  {
+    "revision": "d6b981146e0bad0c932c",
+    "url": "/books-api/static/js/2.0dce98b8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/books-api/static/js/2.0dce98b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d5f38f15ff077c0e28",
+    "url": "/books-api/static/js/main.f5079bac.chunk.js"
+  },
+  {
+    "revision": "402cda36b9f30b3e589e",
+    "url": "/books-api/static/js/runtime-main.292af6d5.js"
+  }
+]);
